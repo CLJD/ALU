3e27876ad7b5441aa142ebbae56ce1ce796eacc2
charje <charles.b.jackson@protonmail.com>
2019-10-15 23:45:06 -0500

    add skeleton of ALU

---
+++ Code/testbench.v
@@ -0,0 +1,10 @@
+
+module ALU(input reg opcode, operand1, operand2, statusIn 
+           output reg result, statusOut);
+   
+endmodule; // ALU
+
+module testbench();
+   
+endmodule; // testbench
+
