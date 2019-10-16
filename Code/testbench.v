2f1615b187b05312765daaf4129c1a8c2ab2ba9b
charje <charles.b.jackson@protonmail.com>
2019-10-16 13:31:12 -0500

    Add more skeletons

--- Code/testbench.v
+++ Code/testbench.v
@@ -1,3 +1,29 @@
+// iverilog
+
+module Add(input reg a, b, cin, 
+           output reg sum, cout);
+
+endmodule; // Add
+
+module Sub(input reg a, b, 
+           output reg difference);
+
+endmodule; // Sub
+
+module Shift(input reg num, shift,
+             output reg shifted);
+   
+endmodule; // Shift
+
+module Mult(input reg a, b,
+            output reg upper, lower);
+   
+endmodule; // Mult
+
+module Div(input reg dividen, divisor,
+           output reg quotient, remainder);
+   
+endmodule; // Div
 
 module ALU(input reg opcode, operand1, operand2, statusIn 
            output reg result, statusOut);
@@ -7,4 +33,3 @@ endmodule; // ALU
 module testbench();
    
 endmodule; // testbench
-
