787d58c074de6cbc473b230117df771002de2ea5
charje <charles.b.jackson@protonmail.com>
2019-10-16 13:43:42 -0500

    Add AddHalf and remove regs

--- Code/testbench.v
+++ Code/testbench.v
@@ -1,35 +1,41 @@
 // iverilog
 
-module Add(input reg a, b, cin, 
-           output reg sum, cout);
+module AddHalf (input a, b, 
+                output sum, c_out);
+   xor G1(sum, a, b);	// Gate instance names are optional
+   and G2(c_out, a, b);
+endmodule // AddHalf
+
+module Add(input a, b, cin, 
+           output sum, cout);
 
 endmodule; // Add
 
-module Sub(input reg a, b, 
-           output reg difference);
+module Sub(input a, b, 
+           output difference);
 
 endmodule; // Sub
 
 /* a shift value greater than 0 will shift right and
  * a shift value less than 0 will shift left
  */ 
-module Shift(input reg num, shift,
-             output reg shifted);
+module Shift(input num, shift,
+             output shifted);
    
 endmodule; // Shift
 
-module Mult(input reg a, b,
-            output reg upper, lower);
+module Mult(input a, b,
+            output upper, lower);
    
 endmodule; // Mult
 
-module Div(input reg dividen, divisor,
-           output reg quotient, remainder);
+module Div(input dividen, divisor,
+           output quotient, remainder);
    
 endmodule; // Div
 
-module ALU(input reg opcode, operand1, operand2, statusIn 
-           output reg result, statusOut);
+module ALU(input opcode, operand1, operand2, statusIn 
+           output result, statusOut);
    
 endmodule; // ALU
 
